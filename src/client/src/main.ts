133df1775a58ea50998883c4c228184861eecc2f
leokolezhukfriss <leo.kolezhuk@friss.com>
2022-08-21 19:05:06 +0200

    Revert "feature: Added pinia"
    This reverts commit dddacaa89bb1d7b4c04c8d2ea1b3cce256399581.

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -5,12 +5,10 @@ import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
 import VueGoogleMaps from "@fawmi/vue-google-maps";
 import { GOOGLE_MAPS_API_KEY } from "./config";
-import { createPinia } from "pinia";
 
 loadFonts();
 
 createApp(App)
-  .use(createPinia())
   .use(router)
   .use(vuetify)
   .use(VueGoogleMaps, {
